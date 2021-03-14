881f8d36ba471b9cbdc98d8b8b314be7fced615b
myrhanda <80593909+myrhanda@users.noreply.github.com>
2021-03-14 13:51:04 +0100

    amélioration de la fonction d'envoi de gémissement lors de l'orgasme
    amélioration de la fonction d'envoi de gémissement lors de l'orgasme

--- js/system/ChatRoom.js
+++ js/system/ChatRoom.js
@@ -39,62 +39,33 @@ function initActivityOrgasmStart(){
 		if(scriptOn && C.MemberNumber==Player.MemberNumber){
 			if(C.ID==0 && C.MemberNumber==Player.MemberNumber){
 				var moan;
-				var backupChatRoomTargetMemberNumber;
+				var backupChatRoomTargetMemberNumber=null;
 				//doit pas se lancer en prive
 				//doit pas se lancer en /me
 				//doit se lancer uniquement en chat simple
 				if(scriptOn){
 					msg=ElementValue("InputChat");
-					if(ChatRoomTargetMemberNumber!=null){
-						backupChatRoomTargetMemberNumber=ChatRoomTargetMemberNumber;
-						ChatRoomTargetMemberNumber=null;
-						ElementValue("InputChat","");
-					}
 					if(isSimpleChat(msg)){
-						if(msg!=null && msg!=""){
-							moan="... "+getOrgasmMoan();
-						}
-						else{
-							moan=msg+"... "+getOrgasmMoan();
-						}
+						
+						moan=msg+"... "+getOrgasmMoan();
+						
 						ElementValue("InputChat",moan);
 						msg="";
 						ChatRoomSendChat();
 					}
 					else{
+						backupChatRoomTargetMemberNumber=ChatRoomTargetMemberNumber;
+						ChatRoomTargetMemberNumber=null;
 						moan="... "+getOrgasmMoan();
 						ElementValue("InputChat",moan);
 						ChatRoomSendChat();
+						ChatRoomTargetMemberNumber=backupChatRoomTargetMemberNumber;
+						ElementValue("InputChat",msg);
 					}
-					ChatRoomTargetMemberNumber=backupChatRoomTargetMemberNumber;
-					ElementValue("InputChat",msg);
 				}
 			}
 		}
 		backupActivityOrgasmStart(C);
-	};
-}
-
-function initActivityOrgasmPrepareOverride(){
-	logDebug("Entree MoanerInitAlteredFns pour ActivityOrgasmPrepare");
-	backupActivityOrgasmPrepare = ActivityOrgasmPrepare;
-	ActivityOrgasmPrepare = (C) => {
-		  
-		//logDebug("Entree ActivityOrgasmPrepare");
-		if(scriptOn && C.MemberNumber==Player.MemberNumber){
-			var moan;
-			if(Player.IsEgged()){
-				moan=getMoan(Math.floor(C.ArousalSettings.Progress / 20),true,Math.random()*1000);
-			}
-			else{
-				moan="Nyaaaaaah!?";
-			}
-			//ca marche mais en edge ca fait vraiment beaucoup
-			 console.log("orgasme detecte pour le joueur");
-			ElementValue("InputChat",moan);
-			//ChatRoomSendChat();
-		}
-		backupActivityOrgasmPrepare(C);
 	};
 }
 
@@ -109,4 +80,3 @@ function stopMoanScript(){
 function isSimpleChat(msg){
 	return !msg.startsWith("/")&&!msg.startsWith("*")&&ChatRoomTargetMemberNumber==null;
 }
-//InitAlteredFns();
